c40c3dec78c5c656eb7a99bef52789bb1f4aab08
Mike Poltyn <mike@poltyn.com>
2011-04-25 12:26:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ValveTime::Application.config.secret_token = 'ee8b37f1e958e8dd62f770ca14d4e2d1e98d68bf9ab89aa347ee957e037a4639b19faecd8bada2caba01a53f60926f1da526e081240b8235b7d9ecbe1c302fbf'
