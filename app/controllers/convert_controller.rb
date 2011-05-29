53159c849075023930c4322861317daa5fe2b254
Mike Poltyn <mike@poltyn.com>
2011-05-29 23:37:57 +0200

    First convert try

--- app/controllers/convert_controller.rb
+++ app/controllers/convert_controller.rb
@@ -1,7 +1,21 @@
 class ConvertController < ApplicationController
 
   def index
-    @name = params[:name]
+    if params[:name]
+      @input = params[:name]
+      date_check = Chronic.parse(@input)
+      if date_check.present?
+        @type = "date"
+        @output = date_check.to_time
+      else
+        @type = "game"
+        @output = giantbomb_search_for(@input).first
+      end
+    end
+    respond_to { |wants|
+      wants.html
+      wants.json { render :text => {:type => @type, :input => @input, :output => @output}.to_json }
+    }
   end
   
   def autocomplete
